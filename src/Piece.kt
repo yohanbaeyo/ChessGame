4513ee75e310091f9c2c091d50bbf87736bb73c9
BaeYohan <yohanbaeyo@gmail.com>
2021-10-31 23:45:23 +0900

    fix import image

--- src/Piece.kt
+++ src/Piece.kt
@@ -7,6 +7,10 @@ enum class PieceType {
     PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, UNDEFINED
 }
 
+fun inputFilePath(fileName:String):String {
+    return Piece::class.java.getResource("$fileName.png")!!.path.replace("%20", " ")
+}
+
 abstract class Piece {
     companion object {
         fun makePiece(pieceType: PieceType = PieceType.UNDEFINED, playerSet: PlayerSet = PlayerSet.UNDEFINED):Piece {
@@ -23,22 +27,21 @@ abstract class Piece {
     }
 
     abstract val playerSet:PlayerSet
-//    abstract val imageBlack: BufferedImage
-//    abstract val imageWhite: BufferedImage
+    abstract val imageBlack: BufferedImage
+    abstract val imageWhite: BufferedImage
 
     abstract fun display(mainPanel: MainFrame.MainPanel, position: Position)
     abstract fun displayMovable(graphics: Graphics, position: Position)
 }
 
 class NoPiece(override val playerSet: PlayerSet):Piece() {
-//    override val imageBlack: BufferedImage
-////        get() = TODO("Not yet implemented")
-//    override val imageWhite: BufferedImage
-////        get() = TODO("Not yet implemented")
-//    init {
-//        imageBlack = ImageIO.read(File(Piece.javaClass!!.classLoader.getResource("PawnBlack.png")!!.path))
-//        imageWhite = ImageIO.read(File(Piece.javaClass!!.classLoader.getResource("PawnWhite.png")!!.path))
-//    }
+    override val imageBlack: BufferedImage
+    override val imageWhite: BufferedImage
+    init {
+//    println(Piece::class.java.getResource("PawnBlack.png")!!.path.replace("%20", " "))
+        imageBlack = ImageIO.read(File(inputFilePath("PawnBlack")))
+        imageWhite = ImageIO.read(File(inputFilePath("PawnWhite")))
+    }
 
     override fun display(mainPanel: MainFrame.MainPanel, position: Position) {
 
@@ -51,13 +54,14 @@ class NoPiece(override val playerSet: PlayerSet):Piece() {
 }
 
 class Pawn(override val playerSet: PlayerSet):Piece() {
-//    override val imageBlack: BufferedImage
-//    override val imageWhite: BufferedImage
-//
-//    init {
-//        imageBlack = ImageIO.read(File(Piece::class.java.getResource("PawnBlack.png")!!.path))
-//        imageWhite = ImageIO.read(File(Piece::class.java.getResource("PawnWhite.png")!!.path))
-//    }
+    override val imageBlack: BufferedImage
+    override val imageWhite: BufferedImage
+
+    init {
+//        println(this::class.java.classLoader.getResource("PawnBlack.png")!!.path)
+        imageBlack = ImageIO.read(File(inputFilePath("PawnBlack")))
+        imageWhite = ImageIO.read(File(inputFilePath("PawnWhite")))
+    }
 
     override fun display(mainPanel: MainFrame.MainPanel, position: Position) {
 //        val g = mainPanel.graphics
